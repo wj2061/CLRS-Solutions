41b7ace60e8490486ac4012e259d7bf7e3ab61e8
wj2061 <wj2061@126.com>
2015-12-20 03:41:30 +0800

    add 3-P

---
+++ chapter3/3_P.playground/Contents.swift
@@ -0,0 +1,61 @@
+//3.P-2
+let a = [true,true,false,false,false]
+let b = [true,true,false,false,false]
+let c = [false,false,false,false,false]
+let d = [false,false,true,true,false]
+let e = [true,false,true,false,true]
+let f = [true,false,true,false,true]
+
+//3.P-3
+/*√
+ 2^{2^{n + 1}} >
+ 2^{2^n}>
+ (n + 1)!>
+ n!>
+ e^n>
+ n ＊（ 2^n ）>
+ 2^n >
+ (3/2)^n >
+（lgn）^{lgn} =
+ n^{lg{lg n}} >
+ lg{n}! >
+ n^3 >
+ n^2 =
+ 4^{lgn} >
+ n*lgn =
+ lg(n!) >
+ n  =
+ 2^{lgn} >
+ (√2)^{lgn}  =
+ √n >
+ 2^{√(2*lgn)} >
+ (lgn)^2 >
+ ln n >
+ √(lgn) >
+ ln (ln n) >
+ 2^{(lg n)^*} >
+ (lg n)^* >
+ (lg (lg n) )^* =
+ lg ((lg n)^*) >
+ n^(1/lgn) >
+ 2 =
+ 1
+*/
+
+//f(n)=1/(1+sinn) if sinn != -1
+//f(n)=1          if sinn == -1
+
+//3.P-6
+/*
+a = n
+b = (lgn)^*
+c = lgn
+d = lgn
+e = lg(lg n)
+f = infty
+g = lg(lg n)
+
+
+
+*/
+
