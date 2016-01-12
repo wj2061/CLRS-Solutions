f68e237b4442e5bc0a8b6d90ffe96af0c65595a8
wj2061 <wj2061@126.com>
2016-01-12 11:50:45 +0800

    add 7_P

--- chapter7/7_P.playground/Contents.swift
+++ chapter7/7_P.playground/Contents.swift
@@ -1,3 +1,4 @@
+import Darwin
 //7.P-1
 //QuickSort
 func partition(inout ay:[Int],p:Int,r:Int)->Int{
@@ -25,7 +26,7 @@ func quicksort(inout ay:[Int],p:Int,r:Int){
     }
 }
 
-var ay = [1,4,23,53,64,86,544,43,54,5345,32,35,3]
+var ay = [1,4,23,53,64,86,544,23,54,5345,23,35,3,7]
 
 quicksort(&ay, p: 0, r: 12)
 
@@ -56,6 +57,136 @@ var ay1 = [1,4,23,53,64,86,544,43,54,5345,32,35,3]
 
 hoareSort(&ay1, p: 0, r: 12)
 
+//7.P-2
+//a  n*lgn
+//b  Quicksort with equal element values
+func equalpartition(inout ay:[Int],p:Int,r:Int)->(Int,Int){
+    var t = 0
+    let x = ay[r]
+    var i = p-1
+    for  j in p..<r{
+        if ay[j] < x{
+            i++
+            if i != j{
+                swap(&ay[i], &ay[j])
+            }
+        }
+        if ay[j] == x {
+            t++
+        }
+    }
+    if i+1 != r{
+        swap(&ay[i+1], &ay[r])
+    }
+    return (i+1-t,i+1)
+}
+
+func randomizedPartition(inout ay:[Int],p:Int,r:Int)->(Int,Int){
+    let  i = random()%(r-p+1) + p
+    if i != r{
+        swap(&ay[i], &ay[r])
+    }
+    let rs = equalpartition(&ay, p: p, r: r)
+    return rs
+}
+
+func equalSort(inout ay:[Int],p:Int,r:Int){
+    if p<r{
+        let q = randomizedPartition(&ay, p: p, r: r)
+        equalSort(&ay, p: p, r: q.0-1)
+        equalSort(&ay, p: q.1+1, r: r)
+    }
+}
+
+var ay2 = [1,4,23,53,64,86,544,23,54,5345,23,35,3,7]
+
+equalSort(&ay2, p: 0, r: 13)
+
+
+//7.P-4
+//b Worst-Case   q = r each time
+
+//c 
+func tailRecursive(inout ay:[Int],var p:Int,r:Int){
+    while p<r{
+        let q = partition(&ay, p: p, r: r)
+        tailRecursive(&ay, p: p, r: q-1)
+        p  = q + 1
+    }
+}
+var ay3 = [1,4,23,53,64,86,544,23,54,5345,23,35,3,7]
+tailRecursive(&ay3,p: 0,r: 13)
+
+func tailRecursive1(inout ay:[Int],var p:Int,var r:Int){
+    while p<r{
+        let q = partition(&ay, p: p, r: r)
+        
+        if q - p < r - q{
+            tailRecursive1(&ay, p: p, r: q-1)
+            p  = q + 1
+        }else{
+            tailRecursive1(&ay, p: q+1, r: r)
+            r  = q - 1
+        }
+    }
+}
+
+var ay4 = [1,4,23,53,64,86,544,23,54,5345,23,35,3,7]
+tailRecursive1(&ay4,p: 0,r: 13)
+
+//7.P-5
+//a  P(i) = 6(i - 1)(n - i)/n(n - 1)(n - 2)
+//b  3/2n
+
+
+//7.P-6
+func intersects(a:(Int,Int),b:(Int,Int))->Bool{
+    return a.0 <= b.1 && b.0 <= a.1
+}
+
+func before(a:(Int,Int),b:(Int,Int))->Bool{
+    return a.1 < b.0
+}
+
+func fuzzyPartition(inout ay:[(Int,Int)],p:Int,var r:Int)->(Int,Int){
+//    let  i = random()%(r-p+1) + p
+//    if i != r{
+//        swap(&ay[i], &ay[r])
+//    }
+    
+    var t = 0
+    let x = ay[r]
+    var i = p-1
+    for  j in p..<r{
+        if ay[j].1 < x.0{
+            i++
+            if i != j{
+                swap(&ay[i], &ay[j])
+            }
+        }
+        if intersects(ay[j], b: x){
+            t++
+        }
+    }
+    if i+1 != r{
+        swap(&ay[i+1], &ay[r])
+    }
+    return (i+1-t,i+1)
+    
+}
+
+func fuzzySort(inout ay:[(Int,Int)],p:Int,r:Int){
+    if p < r {
+        let piovt = fuzzyPartition(&ay, p: p, r: r)
+        print(piovt)
+//        fuzzySort(&ay, p: piovt.0, r: piovt.1)
+        fuzzySort(&ay, p: p, r: piovt.0-1)
+        fuzzySort(&ay, p: piovt.1+1, r: r)
+    }
+}
+
+var ay5 = [(2,4),(0,1),(3,5),(-1,-2),(0,99)]
+fuzzySort(&ay5, p: 0, r: 4)
 
 
 
